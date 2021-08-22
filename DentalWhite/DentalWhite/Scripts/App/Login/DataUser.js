cd50455bb12f942fe49b3ff23586e01fc042dad4
Luiscerda <luiskrlos00231@gmail.com>
2021-08-21 21:23:27 -0500

    log out

---
+++ DentalWhite/DentalWhite/Scripts/App/Login/DataUser.js
@@ -0,0 +1,13 @@
+﻿var DataUser;
+$(document).ready(function () {
+    DataUser = getCookieGeneral('UsuarioDW');
+    DataUser = JSON.parse(Base64.decode(DataUser));
+    $("#cerrarSesion").on("click", function (event) {
+        CerrarSesion();
+    });
+});
+
+function CerrarSesion() {
+    deleteCookie('UsuarioDW');
+    window.location.href = '/Login/Index';
+}
